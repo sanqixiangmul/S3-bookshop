665b797ae25abe2364689987499e9797ff276d2f
wjjkyy <2048809939@qq.com>
2020-09-25 10:04:49 +0800

    修改book、user 中 yml的数据库名  以及在index中添加远程调用

---
+++ S3-bookshop-index/src/main/java/com/yc/crbook/web/remote/IUserAction.java
@@ -0,0 +1,30 @@
+package com.yc.crbook.web.remote;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.yc.crbook.bean.CrCart;
+import com.yc.crbook.bean.CrUser;
+import com.yc.crbook.bean.Result;
+
+@FeignClient("cruser")
+public interface IUserAction {
+	
+	/**
+	 * Feigin 要求对象参数标注 @RequestBody 注解 
+	 */
+	@PostMapping("user/login")
+	Result<CrUser> login(@RequestBody CrUser user);
+	
+	@PostMapping("user/register")
+	Result register(@RequestBody CrUser user);
+	
+	@GetMapping("cart/findByUid")
+	List<CrCart> findByUid(@RequestParam Integer uid);
+
+}
