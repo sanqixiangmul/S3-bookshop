650672bb2b61687061580dc01d21f1767473fbdf
wjjkyy <2048809939@qq.com>
2020-09-26 11:26:28 +0800

    indexAction修改

--- S3-bookshop-index/src/main/java/com/yc/crbook/web/IndexAction.java
+++ S3-bookshop-index/src/main/java/com/yc/crbook/web/IndexAction.java
@@ -9,6 +9,10 @@ public class IndexAction {
 	public String index() {
 		return "index";
 	}
+	@GetMapping("/product")
+	public String product() {
+		return "product";
+	}
 	@GetMapping("/book")
 	public String book() {
 		return "book";
