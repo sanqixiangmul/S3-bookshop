0b2f28526c8e35edcc97e4db83c9d172f47a2614
1079368619 <1079368619@qq.com>
2020-09-27 16:05:03 +0800

    修改IndexAction

--- S3-bookshop-index/src/main/java/com/yc/bookshop/web/IndexAction.java
+++ S3-bookshop-index/src/main/java/com/yc/bookshop/web/IndexAction.java
@@ -1,18 +1,40 @@
 package com.yc.bookshop.web;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.yc.bookshop.bean.Booktype;
+import com.yc.bookshop.web.remote.IBooktypeAction;
 
 @Controller
 public class IndexAction {
+	
+	@Resource
+	private IBooktypeAction btaction;
+	
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
-	@GetMapping("/product")
-	public String product() {
+	
+	@GetMapping(path= {"/toproduct", "product.html"})
+	public String toproduct() {
 		return "product";
 	}
+	
+	@PostMapping("product")
+	public String product(Model m) {
+		List<Booktype> type = btaction.getBooktypes();
+		m.addAttribute("type", type);
+		return "product";
+	}
+	
 	@GetMapping("/book")
 	public String book() {
 		return "book";
