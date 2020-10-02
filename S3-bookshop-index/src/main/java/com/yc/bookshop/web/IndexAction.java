31c7610de56bbc0fce778cd48cdc90bf1b74d491
ASUS <ASUS@DESKTOP-4B1DDFM>
2020-10-02 16:15:36 +0800

    登录，注册

--- S3-bookshop-index/src/main/java/com/yc/bookshop/web/IndexAction.java
+++ S3-bookshop-index/src/main/java/com/yc/bookshop/web/IndexAction.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
